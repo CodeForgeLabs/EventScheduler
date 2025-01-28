5ea1a2059c48ac1de28344d6c4584609d95545d1
lordmhri <meheret.alemu-ug@aau.edu.et>
2025-01-28 11:14:40 +0300

    added eventId

--- event.h
+++ event.h
@@ -6,24 +6,25 @@ using namespace std;
 struct Event
 {
     string name;
+    int id;
     string date;
     string event_time;
     int priority;
 
-    void print();
+    void print() const;
 
     bool operator>(const Event &other) const;
+    bool operator<(const Event &other) const;
     /*
     this is used to overload the ">" operator so when comparing events
     we can compare the events themselves like event1 >event2 instead 
     of event1.priority > event2.priority
     */
-
+    static int nextId;
     static Event AddEvent();
 
-    Event() {}; //default constructor
-
-    Event(string n, string d, string t, int p) : name(n), date(d), event_time(t), priority(p) {};
+    Event(string name = "", string date = "", string event_time = "", int priority = 0)
+        : name(name), date(date), event_time(event_time), priority(priority), id(nextId++) {};
 };
 
 #endif
